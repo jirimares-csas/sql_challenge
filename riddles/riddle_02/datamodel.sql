f4d852532af946f03debf995e3b5a26d39490a2b
jirimares-csas <jirmares@gmail.com>
2026-09-02 07:47:09 +0200

    Update datamodel.sql

--- riddles/riddle_02/datamodel.sql
+++ riddles/riddle_02/datamodel.sql
@@ -1,5 +1,5 @@
 create table polybius_square_ciphers (cipher_id integer, enc_message_cz varchar2(1000), enc_message_en varchar2(1000), enc_password varchar2(50));
-create table polybius_square_cipher_solutions (cipher_id integer, dec_message_cz varchar2(1000), dec_message_en varchar2(1000));
+create table polybius_square_ciphers_solution (cipher_id integer, dec_message_cz varchar2(1000), dec_message_en varchar2(1000));
 
 insert into polybius_square_ciphers (cipher_id, enc_message_cz, enc_message_en, enc_password)
 values
@@ -15,13 +15,13 @@ values
 '52212431123513125245123131252312',
 'VERYVERYLONGPASSWORD');
 
-insert into polybius_square_cipher_solutions (cipher_id, dec_message_cz, dec_message_en)
+insert into polybius_square_ciphers_solution (cipher_id, dec_message_cz, dec_message_en)
 values
 (1,
 'ZPRAVA',
 'MESSAGE');
 
-insert into polybius_square_cipher_solutions (cipher_id, dec_message_cz, dec_message_en)
+insert into polybius_square_ciphers_solution (cipher_id, dec_message_cz, dec_message_en)
 values
 (2,
 'PRISNETAINAZPRAVA',
